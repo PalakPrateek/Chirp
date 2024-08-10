47e33cab2825b7c3648b05ed3937828c267bd912
PalakPrateek <palak15408@gmail.com>
2024-08-10 23:52:52 +0530

    first commit

---
+++ frontend/src/components/miscellaneous/ProfileModal.js
@@ -0,0 +1,74 @@
+import { ViewIcon } from "@chakra-ui/icons";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+  IconButton,
+  Text,
+  Image,
+} from "@chakra-ui/react";
+
+const ProfileModal = ({ user, children }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      {children ? (
+        <span onClick={onOpen}>{children}</span>
+      ) : (
+        <IconButton
+          color="black"
+          d={{ base: "flex" }}
+          icon={<ViewIcon />}
+          onClick={onOpen}
+        />
+      )}
+      <Modal size="lg" onClose={onClose} isOpen={isOpen} isCentered>
+        <ModalOverlay />
+        <ModalContent bg="#2c333a" h="410px" textColor="white">
+          <ModalHeader
+            fontSize="40px"
+            fontFamily="Roboto Slab"
+            d="flex"
+            justifyContent="center"
+          >
+            {user.name}
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody
+            d="flex"
+            flexDir="column"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Image
+              borderRadius="full"
+              boxSize="150px"
+              src={user.pic}
+              alt={user.name}
+            />
+            <Text
+              fontSize={{ base: "28px", md: "30px" }}
+              fontFamily="Roboto Slab"
+            >
+              Email: {user.email}
+            </Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button textColor="black" onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ProfileModal;
